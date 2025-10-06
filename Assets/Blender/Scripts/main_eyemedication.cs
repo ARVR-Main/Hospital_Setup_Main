bae6e8e97200c02ac6f3a6ef8574b53a6cd83b4b
abineshsrinivasan <abinesh701032gmail.com>
2025-10-06 17:04:03 +0530

    revised info

--- Assets/Blender/Scripts/main_eyemedication.cs
+++ Assets/Blender/Scripts/main_eyemedication.cs
@@ -10,8 +10,6 @@ using TMPro;
 
 public class main_eyemedication : MonoBehaviour
 {
-
-
     [Header("Scripts")]
     public StartKeyHoldController startKeyHoldController;
     public WaterTapController waterTapController;
@@ -24,50 +22,27 @@ public class main_eyemedication : MonoBehaviour
     public GameObject WaterInfo;
     public GameObject EyeDropBottleInfo;
     public GameObject DropingInfo;
-
-
-
     public Grabbable eyeDropGrab; // assign in Inspector
-    public GameObject eyeTarget;  // assign in Inspector (could be a small trigger collider)
-
+    public GameObject eyeTarget; // assign in Inspector (could be a small trigger collider)
 
     [Header("Medical Items & Triggers")]
     public GameObject _gloveBox;
-    
-
-
-
 
     [Header("Annotations")]
-
     public List<GameObject> Annotations;
-
 
     [Serializable]
     class Steps
     {
-        [SerializeField]
-        public VideoClip _clip;
-
-        [SerializeField]
-        public string _description;
+        [SerializeField] public VideoClip _clip;
+        [SerializeField] public string _description;
     }
 
-    [SerializeField]
-    public VideoPlayer _descriptionVideoPlayer;
-
-    [SerializeField]
-    public TextMeshProUGUI _descriptionText;
-
-
-    [SerializeField]
-    List<Steps> m_StepList = new List<Steps>();
+    [SerializeField] public VideoPlayer _descriptionVideoPlayer;
+    [SerializeField] public TextMeshProUGUI _descriptionText;
+    [SerializeField] List<Steps> m_StepList = new List<Steps>();
 
     int m_CurrentStepIndex = 0;
-
-
-
-
     IEnumerator activeThread;
 
     public enum VenipuntureSteps
@@ -79,7 +54,6 @@ public class main_eyemedication : MonoBehaviour
         applyEyeDrop,
         endTrigger,
     }
-
 
     private VenipuntureSteps activeStep = 0;
 
@@ -107,6 +81,7 @@ public class main_eyemedication : MonoBehaviour
             Annotations[i].SetActive(i == index);
         }
     }
+
     public void Next()
     {
         if (m_StepList == null || m_StepList.Count == 0)
@@ -122,23 +97,18 @@ public class main_eyemedication : MonoBehaviour
         _descriptionText.text = m_StepList[m_CurrentStepIndex]._description;
     }
 
-
-
     private void Start()
     {
         VenipuntureManager(0);
-
         _gloveBox.GetComponent<Collider>().enabled = false;
         GloveInfo.SetActive(false);
         WaterInfo.SetActive(false);
         EyeDropBottleInfo.SetActive(false);
         DropingInfo.SetActive(false);
 
-        // Disable grabbing initially
         if (eyeDropGrab != null)
             eyeDropGrab.enabled = false;
     }
-
 
     public void Update()
     {
@@ -148,28 +118,25 @@ public class main_eyemedication : MonoBehaviour
         }
     }
 
-
     public void VenipuntureManager(int newStep = -1)
     {
-
         if (activeThread != null)
         {
             StopCoroutine(activeThread);
         }
+
         if (newStep == -1)
         {
             activeStep++;
         }
         else
         {
-
             activeStep = (VenipuntureSteps)newStep;
         }
 
         switch (activeStep)
         {
             case VenipuntureSteps.startTrigger:
-
                 Debug.Log(activeStep);
                 Debug.Log(m_CurrentStepIndex);
                 StartTriggerVerifier();
@@ -180,10 +147,8 @@ public class main_eyemedication : MonoBehaviour
                 Debug.Log(m_CurrentStepIndex);
                 Annotator(0);
                 Debug.Log(activeStep);
-                
                 HandwashTriggerVerifier();
                 break;
-
 
             case VenipuntureSteps.glovesTrigger:
                 Annotator(1);
@@ -193,64 +158,43 @@ public class main_eyemedication : MonoBehaviour
                 _gloveBox.GetComponent<Collider>().enabled = true;
                 Debug.Log(activeStep);
                 glovesTriggerVerifier();
-                
                 break;
-
 
             case VenipuntureSteps.takeEyeDrop:
                 Debug.Log("Step: Take the eye drop bottle");
-
-                Annotator(2); // show annotation
+                Annotator(2);
                 GloveInfo.SetActive(false);
                 WaterInfo.SetActive(false);
                 EyeDropBottleInfo.SetActive(true);
 
-                // Enable grabbing
                 if (eyeDropGrab != null)
                     eyeDropGrab.enabled = true;
 
-                // Start verification coroutine
                 activeThread = TakeEyeDropVerifierAction();
                 StartCoroutine(activeThread);
                 break;
 
-
             case VenipuntureSteps.applyEyeDrop:
                 Debug.Log("Step: Apply eye drops");
-
-                Annotator(3); // annotation for apply step
+                Annotator(3);
                 GloveInfo.SetActive(false);
                 WaterInfo.SetActive(false);
                 EyeDropBottleInfo.SetActive(false);
                 DropingInfo.SetActive(true);
 
-                // Optionally enable interaction with the eyeTarget
                 if (eyeTarget != null)
                     eyeTarget.GetComponent<Collider>().enabled = true;
 
-                // Start coroutine to verify application
                 activeThread = ApplyEyeDropVerifierAction();
                 StartCoroutine(activeThread);
                 break;
-
-
-
         }
-
-
     }
-
-
-
-
-
-
 
     public void StartTriggerVerifier()
     {
         activeThread = StartTriggerVerifierAction();
         StartCoroutine(StartTriggerVerifierAction());
-
     }
 
     private IEnumerator StartTriggerVerifierAction()
@@ -263,23 +207,15 @@ public class main_eyemedication : MonoBehaviour
             }
             yield return new WaitForFixedUpdate();
         }
+
         Next();
-
         VenipuntureManager();
-
-
     }
-
-
-
-
-
 
     private void HandwashTriggerVerifier()
     {
         activeThread = HandwashTriggerVerifierAction();
         StartCoroutine(HandwashTriggerVerifierAction());
-
     }
 
     private IEnumerator HandwashTriggerVerifierAction()
@@ -292,24 +228,16 @@ public class main_eyemedication : MonoBehaviour
             }
             yield return new WaitForFixedUpdate();
         }
-        /*waterTapController.gameObject.GetComponent<BoxCollider>().enabled = false;*/
 
         destroyer.DestroyComp(waterTapController.gameObject.GetComponent<WaterTapController>());
         Next();
         VenipuntureManager();
-
     }
-
-
-
-
-
 
     private void glovesTriggerVerifier()
     {
         activeThread = glovesTriggerverifierAction();
         StartCoroutine(glovesTriggerverifierAction());
-
     }
 
     private IEnumerator glovesTriggerverifierAction()
@@ -322,6 +250,7 @@ public class main_eyemedication : MonoBehaviour
             }
             yield return new WaitForFixedUpdate();
         }
+
         destroyer.DestroyComp(gloveController.gameObject.GetComponent<MedicalGloveController>());
         Next();
         VenipuntureManager();
@@ -334,18 +263,14 @@ public class main_eyemedication : MonoBehaviour
             if (eyeDropGrab != null && eyeDropGrab.BeingHeld)
             {
                 Debug.Log("Eye drop bottle picked!");
-
-                
-                break; // exit loop
+                break;
             }
-
             yield return new WaitForFixedUpdate();
         }
 
-        Next();                // move to next description step
-        VenipuntureManager();  // move to applyEyeDrop step
+        Next();
+        VenipuntureManager();
     }
-
 
     private IEnumerator ApplyEyeDropVerifierAction()
     {
@@ -353,23 +278,18 @@ public class main_eyemedication : MonoBehaviour
 
         while (!applied)
         {
-            // Replace this with your real check
-            // Example: player drops liquid into the eye collider
             if (/* player applies drops to eyeTarget */ false)
             {
                 applied = true;
                 Debug.Log("Eye drops applied!");
 
-                // Disable collider to prevent repeated application
                 if (eyeTarget != null)
                     eyeTarget.GetComponent<Collider>().enabled = false;
             }
-
             yield return new WaitForFixedUpdate();
         }
 
         Next();
-        VenipuntureManager(); // go to next step
+        VenipuntureManager();
     }
-
 }
